ef5eea4f04f037a6eecfc7741c6fb303ccc8d292
River <+>
2022-04-26 11:11:01 -0400

    commit tree a1f28a46985f8c22b823c01f88a66399eb237db7

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift()
+}
+
+function appendCat(name) {
+    let newCats = [...cats]; 
+    newCats.push("Broom")
+    return newCats
+}
+
+function prependCat(name) {
+    let newCats = [...cats];
+    newCats.unshift("Arnold")
+    return newCats
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
